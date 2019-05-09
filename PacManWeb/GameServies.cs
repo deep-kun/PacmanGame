82bcb965bcbb880fe8b6451772284c307183969f
deep-kun <deep9751@gmail.com>
2019-05-09 14:09:05 +0300

    Add files via upload

---
+++ PacManWeb/GameServies.cs
@@ -0,0 +1,11 @@
+﻿using PacManLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace PacManWeb
+{
+    public class GameServies
+    {
+        public  Dictionary<Guid, Game> Games { get; set; } = new Dictionary<Guid, Game>();
+    }
+}
